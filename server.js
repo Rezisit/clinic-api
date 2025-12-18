252dee2b4c08a437a16ff4d702a35382bec6d0c0
Johnrey <rey.gobris.4@gmail.com>
2025-12-19 01:41:41 +0800

    `

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+import express from "express";
 
-mongoose.connect(process.env.MONGO_URI)
+dotenv.config(); // 👈 MUST BE AT THE TOP
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .catch((err) => {
+    console.error("MongoDB error:", err.message);
+    process.exit(1);
+  });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
