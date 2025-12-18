f810d86627203a13cec115516955f30f0337cbd4
Johnrey <rey.gobris.4@gmail.com>
2025-12-19 01:31:33 +0800

    `

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const app = require('./app');
+import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:${PORT}');
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
